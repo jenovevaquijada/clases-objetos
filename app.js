2139f2b202e5728a29cad2f309dfceddfd12bf10
Jenoveva <jenovevaquijada@gmail.com>
2026-01-01 21:47:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Alumno {
-  constructor(nombre, edad, carrera) {
+  constructor(nombre, edad, carrera, institucion) {
     this.nombre = nombre;
     this.edad = edad;
     this.carrera = carrera;
@@ -10,35 +10,56 @@ class Alumno {
   }
 }
 
+const yo = new Alumno("Jenoveva", 38, "Desarrollo Full Stack");
+yo.mostrarInfo();
+
+
 class BandaMusical {
   constructor(nombre, genero, integrantes, discos) {
     this.nombre = nombre;
     this.genero = genero;
     this.integrantes = integrantes;
-    this.discos = discos; // Array
+    this.discos = discos;
   }
 
   mostrarInfo() {
-    console.log(`Banda: ${this.nombre}, Género: ${this.genero}, Integrantes: ${this.integrantes}`);
+    console.log(`La banda ${this.nombre} toca ${this.genero} y tiene ${this.integrantes} integrantes.`);
   }
 
   listarDiscos() {
-    console.log("Discos:", this.discos.join(", "));
+    console.log(`💿 Discos de ${this.nombre}:`);
+    this.discos.forEach((disco, index) => {
+      console.log(`${index + 1}. ${disco}`);
+    });
   }
 }
 
-class Perro {
-  constructor(nombre, raza, edad) {
-    this.nombre = Sakura;
-    this.raza = mestiza;
-    this.edad = 10;
+const miBanda = new BandaMusical(
+  "Oasis", "Britpop", 5, ["Definitely Maybe", "(What's the story) Morning Glory", "Be Here Now"]);
+
+miBanda.mostrarInfo();
+miBanda.listarDiscos();
+
+
+  class Perro {
+  constructor(nombre, raza, edad, esRescatado) {
+    this.nombre = nombre;
+    this.raza = raza;
+    this.edad = edad;
+    this.esRescatado = esRescatado; 
   }
 
   mostrarInfo() {
-    console.log(`Nombre: ${this.nombre}, Raza: ${this.raza}, Edad: ${this.edad}`);
+    const estado = this.esRescatado ? "es una rescatada valiente" : "tiene hogar";
+    console.log(`🐾 ${this.nombre} tiene ${this.edad} años, es de raza ${this.raza} y ${estado}.`);
   }
 
   ladrar() {
-    console.log("¡Guau guau!");
+    console.log(`¡${this.nombre} dice: Guau guau! 🐕✨`);
   }
 }
+
+const molly = new Perro("Molly", "Quiltrita (única)", 8, true);
+
+molly.mostrarInfo();
+molly.ladrar();
